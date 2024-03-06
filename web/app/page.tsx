cd2e8290d7cb75d669ad846eab1e4d9b173a8c2d
Nathan Lazo <84787060+NathanLazo@users.noreply.github.com>
2024-03-05 23:11:10 -0600

    DEPLOY

---
+++ web/app/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect } from 'react';
+import styles from './page.module.css';
+
+function Page() {
+  useEffect(() => {
+    // Dynamically load the Jupiter script
+    const script = document.createElement('script');
+    script.src = 'https://terminal.jup.ag/main-v2.js';
+    script.onload = () => launchJupiter(); // Initialize Jupiter after the script loads
+    document.head.appendChild(script);
+  }, []);
+
+  function launchJupiter() {
+    if (window.Jupiter) {
+      window.Jupiter.init({
+        displayMode: 'integrated',
+        integratedTargetId: 'integrated-terminal',
+        endpoint:
+          'https://mainnet.helius-rpc.com/?api-key=d207dd7f-4f9c-4232-aefd-1ad871e07b8a',
+        strictTokenList: false,
+        defaultExplorer: 'SolanaFM',
+        formProps: {
+          initialAmount: '888888880000',
+          initialInputMint: 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263',
+          initialOutputMint: 'AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR',
+        },
+      });
+    } else {
+      console.error('Jupiter script not loaded yet');
+    }
+  }
+  return (
+    <div className={styles.body}>
+      <div id="integrated-terminal"></div>
+    </div>
+  );
+}
+
+export default Page;
